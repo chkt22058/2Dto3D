a59b23f31ab595a5a1e3c6d952c7ddd72a066109
maruCHS <chat22014@g.nihon-u.ac.jp>
2024-08-11 12:09:04 +0900

    update module file

--- lib/Cvt3dMod.py
+++ lib/Cvt3dMod.py
@@ -16,11 +16,6 @@ import rembg
 
 import subprocess
 
-def run_a_py_with_args(args):
-    command = ["python", "A.py"] + args
-    result = subprocess.run(command, capture_output=True, text=True)
-    
-
 
 
 #dreamgaussian--------------------------------------
@@ -54,31 +49,4 @@ def gen_obj(args) :
         print(result.stderr)
 '''
 
-#bpy-----------------
-import bpy
 
-def import_model(filepath):
-    bpy.ops.import_scene.obj(filepath=filepath)
-
-def create_armature():
-    bpy.ops.object.armature_add()
-
-def auto_rig():
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.object.mode_set(mode='OBJECT')
-    bpy.ops.object.parent_set(type='ARMATURE_AUTO')
-
-def save_file(filepath):
-    bpy.ops.wm.save_as_mainfile(filepath=filepath)
-
-def main():
-    model_path = "path/to/your/model.obj"
-    output_path = "path/to/save/your/rigged_model.blend"
-    
-    import_model(model_path)
-    create_armature()
-    auto_rig()
-    save_file(output_path)
-
-if __name__ == "__main__":
-    main()
