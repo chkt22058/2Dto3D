237d11060bcac6fa0d541c07bf17a951e503e8cc
nanako20031112 <hsjyr1112@i.softbank.jp>
2024-08-11 23:43:14 +0900

    ボタン

--- main.py
+++ main.py
@@ -17,3 +17,46 @@ def main():
 
 if __name__ == "__main__":
     main()
+    import ipywidgets as widgets
+from IPython.display import display
+
+# 各ボタンがクリックされたときの動作
+def on_button1_click(b):
+    print("ボタン1がクリックされました！")
+
+def on_button2_click(b):
+    print("ボタン2がクリックされました！")
+
+def on_button3_click(b):
+    print("ボタン3がクリックされました！")
+
+# ボタン1を作成
+button1 = widgets.Button(
+    description="入力開始",
+    tooltip='ボタン1をクリック',
+    button_style='primary'
+)
+button1.on_click(on_button1_click)
+
+# ボタン2を作成
+button2 = widgets.Button(
+    description="入力完了",
+    tooltip='ボタン2をクリック',
+    button_style='success'
+)
+button2.on_click(on_button2_click)
+
+# ボタン3を作成
+button3 = widgets.Button(
+    description="戻る",
+    tooltip='ボタン3をクリック',
+    button_style='danger'
+)
+button3.on_click(on_button3_click)
+
+# ボタンを横並びに配置するためのボックスを作成
+box = widgets.HBox([button1, button2, button3])
+
+# ボタンを表示
+display(box)
+
