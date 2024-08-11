5a9894e738169816c81dac2c49a66fcb378a2cd3
chkt22058 <chkt22058@g.nihon-u.ac.jp>
2024-08-11 20:53:23 +0900

    change main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello World')
+from lib import Cvt3dMod as cvt
+from lib import Rigging as rig
+
+def main(): 
+
+    # 2D→3D 
+    image_2d = "lib/dreamgaussian-main/data/test.png"
+    cvt.preprocess(image_2d)
+    cvt.training_gaussian("data/image_2d_rgba.png")
+    #cvt.training_mesh("data/image_2d_rgba.png")
+
+
+    # 3D→3Dリギング
+    image_3d = "data/image_2d.obg"
+    rig.main(image_3d)
+    
+
+if __name__ == "__main__":
+    main()
