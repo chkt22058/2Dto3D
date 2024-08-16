bc7f50fae3ce2d658f84698a1ac976e2627ab83a
chkt22058 <chkt22058@g.nihon-u.ac.jp>
2024-08-16 10:45:12 +0900

    sort main

--- main.py
+++ main.py
@@ -3,25 +3,7 @@ from lib import Rigging as rig
 from IPython.display import display
 import ipywidgets as widgets
 
-def main(): 
-
-    # 2D→3D 
-    image_2d = "lib/dreamgaussian-main/data/test.png"
-    cvt.preprocess(image_2d)
-    cvt.training_gaussian("data/image_2d_rgba.png")
-    #cvt.training_mesh("data/image_2d_rgba.png")
-
-
-    # 3D→3Dリギング
-    image_3d = "data/image_2d.obg"
-    rig.main(image_3d)
-
-    print()
-    
-
-if __name__ == "__main__":
-    main()
-    # 各ボタンがクリックされたときの動作
+# 各ボタンがクリックされたときの動作
 def on_button1_click(b):
     print("ボタン1がクリックされました！")
 
@@ -31,35 +13,47 @@ def on_button2_click(b):
 def on_button3_click(b):
     print("ボタン3がクリックされました！")
 
-# ボタン1を作成
-button1 = widgets.Button(
-    description="入力開始",
-    tooltip='ボタン1をクリック',
-    button_style='primary'
-)
-button1.on_click(on_button1_click)
+if __name__ == "__main__":
+    # ボタン1を作成
+    button1 = widgets.Button(
+        description="入力開始",
+        tooltip='ボタン1をクリック',
+        button_style='primary'
+    )
 
-# ボタン2を作成
-button2 = widgets.Button(
-    description="入力完了",
-    tooltip='ボタン2をクリック',
-    button_style='success'
-)
-button2.on_click(on_button2_click)
+    # ボタン2を作成
+    button2 = widgets.Button(
+        description="入力完了",
+        tooltip='ボタン2をクリック',
+        button_style='success'
+    )
 
-# ボタン3を作成
-button3 = widgets.Button(
-    description="戻る",
-    tooltip='ボタン3をクリック',
-    button_style='danger'
-)
-button3.on_click(on_button3_click)
+    #ボタン3を作成
+    button3 = widgets.Button(
+        description="戻る",
+        tooltip='ボタン3をクリック',
+        button_style='danger'
+    )
 
-# ボタンを横並びに配置するためのボックスを作成
-box = widgets.HBox([button1, button2, button3])
+    # 2D→3D 
+    image_2d = "lib/dreamgaussian-main/data/test.png"
+    cvt.preprocess(image_2d)
+    cvt.training_gaussian("data/image_2d_rgba.png")
+    #cvt.training_mesh("data/image_2d_rgba.png")
 
-# ボタンを表示
-display(box)
+    # 3D→3Dリギング
+    image_3d = "data/image_2d.obg"
+    rig.main(image_3d)
+
+    button1.on_click(on_button1_click)
+    button2.on_click(on_button2_click)
+    button3.on_click(on_button3_click)
+
+    # ボタンを横並びに配置するためのボックスを作成
+    box = widgets.HBox([button1, button2, button3])
+
+    # ボタンを表示
+    display(box)
 
 
 
